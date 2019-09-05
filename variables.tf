cb39bd289f928fb7a6b025321f10df80624f6152
wyjung88 <32329787+wyjung88@users.noreply.github.com>
2019-09-05 17:14:59 -0400

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,15 @@
+variable "s3_state" {
+  type = "string"
+}
+
+variable "home_dir" {
+  type = "string"
+}
+variable "engine_type" {
+  type = "string"
+  default = "sqlserver-se"
+}
+
+variable "region" {
+  type = "string"
+}
