aa6e524428e20f9321ef4ec9f1384108278c7629
wyjung88 <32329787+wyjung88@users.noreply.github.com>
2019-09-09 10:41:27 -0400

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -13,3 +13,19 @@ variable "engine_type" {
 variable "region" {
   type = "string"
 }
+
+variable "aws_profile" {
+  type = "string"
+}
+variable "vpc_name" {
+  type = "string"
+}
+variable "aws_region" {
+  type = "string"
+ }
+variable "cidr_block" {
+  type = "string"
+ }
+variable "num_nat_gateways" {
+  type = "string"
+ }
